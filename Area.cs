cea0711b7d7f22e0d888f108e3569a139c628d85
Laqah <laqahraca@protonmail.com>
2022-01-06 23:11:12 +0900

    캐릭터 좌우 이동시 맵을 자동 생성, 삭제하는 기능을 추가한다
    캐릭터의 양방항에서 캐릭터와의 거리 차이를 구하는 식을 넣었더니
    앞 뒤 구역이 모두 거리 조건에 걸렸다.
    캐릭터의 방향을 조건에 거는 방식으로 해결했다.
    캐릭터의 방향을 찾는 근거로는 최대한 간단한 수치를 가져오고자 했다.
    빠른 해결 방식을 채택하면서 코드의 중복이 발생했다.
    
    다른 해결방법으로는
    캐릭터의 위치를 구역 중앙에서 어긋나게 해 수치를 절묘히 맞추는 방법도 있겠다.
    혹은 거리 차이의 절댓값을 이용하는 방법도 있겠다.
    
    캐릭터가 어느 구역에 서있는지 구역인덱스를 구할 수 있다면
    더 쉬운 방법이 보일지도 모르겠다.
    
    남아있는 과제로는 맵이 회귀해야한다는 점이 있다.

--- Area.cs
+++ Area.cs
@@ -3,21 +3,35 @@
 public class Area : MonoBehaviour
 {
 
-    [ SerializeField] private float destroyDistance = 15;
+    [SerializeField] private float destroyDistance;
     private AreaSpawner areaSpawner;
     private Transform playerTransform;
 
-    public void Setup(AreaSpawner areaSpawner, Transform playerTransform) {
+    [SerializeField]
+    private int areaIndex;
+
+    public void Setup(AreaSpawner areaSpawner, Transform playerTransform, int areaIndex) {
         this.areaSpawner = areaSpawner;
         this.playerTransform = playerTransform;
+        this.areaIndex = areaIndex;
+
+        Debug.Log("캐릭터 : " + playerTransform.position.x + " 맵 : " + this.transform.position.x);
     }
 
     private void Update() {
-        if(playerTransform.position.x - transform.position.x >= destroyDistance) {
-            areaSpawner.SpawnArea();
+        //캐릭터가 x 증가 방향으로 이동하는 경우 뒤의 구역을 제거 (뒤의 구역 기준)
+            if(playerTransform.localScale.x >= 0 && playerTransform.position.x - transform.position.x >= destroyDistance) { //당연히 >0
+                areaSpawner.SpawnAreaRight(areaIndex);
 
-            Destroy(gameObject);
-        }
+                Destroy(gameObject);
+            }
+         else 
+            if(playerTransform.localScale.x <= 0 && playerTransform.position.x - transform.position.x <= -1f * destroyDistance) { //당연히 < 0
+                areaSpawner.SpawnAreaLeft(areaIndex);
+
+                Destroy(gameObject);
+            }
+        
     }
 
 }
--- AreaSpawner.cs
+++ AreaSpawner.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class AreaSpawner : MonoBehaviour
 {
     [SerializeField] private GameObject[] areaPrefabs;
-    [SerializeField] private int spawnAreaCountStart = 3;
+    [SerializeField] private int spawnAreaCountStart;
 
     [SerializeField] private float xDistance = 20; //구역 사이의 거리
 
@@ -19,14 +19,25 @@ public class AreaSpawner : MonoBehaviour
 
 
     private void Awake() {
-        for(int i = 0; i < spawnAreaCountStart; ++i) {
-            if(i == 0) {
-                SpawnArea();
-            }
-            else {
-                SpawnArea();
-            }
-        }
+        // for(int i = 0; i < spawnAreaCountStart; ++i) {
+        //     // if(i == 0) {
+        //     //     SpawnArea();
+        //     // }
+        //     // else {
+        //     //     SpawnArea();
+        //     // }
+            
+        // }
+        SpawnArea(0);
+        SpawnArea(1);
+        SpawnArea(-1);
+        SpawnArea(2);
+        SpawnArea(-2);
+    
+    }
+
+    public int GetSpawnAreaCountStart() {
+        return spawnAreaCountStart;
     }
 
     public void SpawnArea() {
@@ -37,9 +48,51 @@ public class AreaSpawner : MonoBehaviour
         clone.transform.position = new Vector3(areaIndex * xDistance * size, ySet, 0);
         clone.transform.localScale = new Vector3(size, size, 1f);
 
-        clone.GetComponent<Area>().Setup(this, playerTransform);
+        clone.GetComponent<Area>().Setup(this, playerTransform, areaIndex);
 
         areaIndex ++;
+
+        //실시간 생성이 아니라 미리 생성해두고 그걸 하나씩 꺼내와도 좋겠다.
+    }
+    public void SpawnArea(int areaIndex) {
+        //지역 인덱스에 구역을 생성한다.
+        GameObject clone = null;
+
+        clone = Instantiate(areaPrefabs[0]);
+
+        clone.transform.position = new Vector3(areaIndex * xDistance * size, ySet, 0);
+        clone.transform.localScale = new Vector3(size, size, 1f);
+
+        clone.GetComponent<Area>().Setup(this, playerTransform, areaIndex);
+
+        //실시간 생성이 아니라 미리 생성해두고 그걸 하나씩 꺼내와도 좋겠다.
+    }
+    public void SpawnAreaRight(int areaIndex) {
+        //지역 인덱스에 구역을 생성한다.
+        GameObject clone = null;
+        int newAreaIndex = areaIndex + spawnAreaCountStart;
+
+        clone = Instantiate(areaPrefabs[0]);
+
+        clone.transform.position = new Vector3(newAreaIndex * xDistance * size, ySet, 0);
+        clone.transform.localScale = new Vector3(size, size, 1f);
+
+        clone.GetComponent<Area>().Setup(this, playerTransform, newAreaIndex);
+
+        //실시간 생성이 아니라 미리 생성해두고 그걸 하나씩 꺼내와도 좋겠다.
+    }
+
+    public void SpawnAreaLeft(int areaIndex) {
+        //지역 인덱스에 구역을 생성한다.
+        GameObject clone = null;
+        int newAreaIndex = areaIndex - spawnAreaCountStart;
+
+        clone = Instantiate(areaPrefabs[0]);
+
+        clone.transform.position = new Vector3(newAreaIndex * xDistance * size, ySet, 0);
+        clone.transform.localScale = new Vector3(size, size, 1f);
+
+        clone.GetComponent<Area>().Setup(this, playerTransform, newAreaIndex);
 
         //실시간 생성이 아니라 미리 생성해두고 그걸 하나씩 꺼내와도 좋겠다.
     }
